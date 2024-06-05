50d52db4a22a5cebd704e0404817297e83db7ecb
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-06-05 15:04:21 +0200

    added docstrings

--- todo app/app.py
+++ todo app/app.py
@@ -8,18 +8,32 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
+    """
+    Todo model representing a task in the todo list.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
     complete = title = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
+    """
+    Renders the main page with the list of all todo tasks.
+
+    :return: Rendered HTML template for the main page.
+    """
 
     list_of_activities = Todo.query.all()
     return render_template('base.html', list_of_activities=list_of_activities)
 
 @app.route('/add', methods=['POST'])
 def add_a_todo_task():
+    """
+    Adds a new todo task to the list.
+
+    :return: Redirects to the main page.
+    """
 
     title = request.form.get('title')
     new_todo_task = Todo(title=title, complete=False)
@@ -29,6 +43,12 @@ def add_a_todo_task():
 
 @app.route('/update/<int:todo_id>')
 def update_a_todo_task(todo_id):
+    """
+    Toggles the completion status of a todo task.
+
+    :param todo_id: ID of the todo task to update.
+    :return: Redirects to the main page.
+    """
 
     todo_task = Todo.query.filter_by(id = todo_id).first()
     todo_task.complete = not todo_task.complete
@@ -37,6 +57,12 @@ def update_a_todo_task(todo_id):
 
 @app.route('/update/<int:todo_id>')
 def delete_a_todo_task(todo_id):
+    """
+    Deletes a todo task from the list.
+
+    :param todo_id: ID of the todo task to delete.
+    :return: Redirects to the main page.
+    """
 
     todo_task = Todo.query.filter_by(id = todo_id).first()
     db.session.delete(todo_task)
@@ -45,6 +71,12 @@ def delete_a_todo_task(todo_id):
 
 @app.route('/update/<int:todo_id>', methods=["GET", "POST"])
 def edit_a_todo_task(todo_id):
+    """
+    Edits the title of a todo task.
+
+    :param todo_id: ID of the todo task to edit.
+    :return: Redirects to the main page after editing or renders the edit page.
+    """
 
     todo_task = Todo.query.get_or_404(todo_id)
     
@@ -57,7 +89,12 @@ def edit_a_todo_task(todo_id):
 
 @app.route('/base')
 def base():
+    """
+    Renders the base page with the list of all todo tasks.
 
+    :return: Rendered HTML template for the base page.
+    """
+    
     list_of_activities = Todo.query.all()
     return render_template('base.html', list_of_activities=list_of_activities)
 
